fc0e3576b548d7499c87035df107eec4a0ec1df7
Ruslan <rusgil@mail.ru>
2022-04-21 00:41:19 +0300

    move db file. And readme edit.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ExpressJS-SQLite-test
 Test project
+
+Commands:
+install: npm i
+run: npx ts-node index
+
+open "http://localhost:3003"
--- db/useDb.ts
+++ db/useDb.ts
@@ -1,6 +1,6 @@
 import sqlite3 from 'sqlite3'
 
-  export const db = new sqlite3.Database('./db.db', (err) => {
+  export const db = new sqlite3.Database('./db/db.db', (err) => {
     if (err) {
       console.log('Could not connect to database', err)
     } else {
